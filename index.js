5590a8db53c06bf29c938e01d7d4d1e0bd703d67
KabirSinghMehrok <mehrok.kabir.singh@gmail.com>
2023-03-05 22:45:37 +0530

    Initialize repository, create basic webpage with routing

---
+++ index.js
@@ -0,0 +1,60 @@
+const livereload = require("livereload");
+const express = require('express');
+const path = require('path')
+const app = express();
+
+// livereload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+// connect livereload using express middleware
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+// avoid infinite loop
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+
+// app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes for home.html and about.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/unga', (req, res) => {
+  res.sendFile(__dirname + '/views/unga.html');
+});
+
+app.get('/unsc', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.get('/g20', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.get('/uncsw', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.get('/unhrc', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.get('/aippm', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/views/404.html');
+});
+
+app.listen(8000, () => {
+  console.log('Listening on 8000');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./**/*.{html,js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: []
+}
